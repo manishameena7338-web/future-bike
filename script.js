e50a27362b168ab16df7850d6ac673d26a437c26
Krishna MEENA <manishameena7338@gmail.com>
2026-04-11 19:21:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const bike = document.querySelector(".bike");
-
-bike.addEventListener("mouseover", () => {
-  bike.style.transform = "rotateY(180deg)";
-});
-
-bike.addEventListener("mouseout", () => {
-  bike.style.transform = "rotateY(0deg)";
-});
